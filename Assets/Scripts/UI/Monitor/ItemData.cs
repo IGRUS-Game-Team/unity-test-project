96c55e76fc8923e666266322dc99ec356c8a393e
jeongminb933 <65400179+jeongminb933@users.noreply.github.com>
2025-07-22 18:04:03 +0900

    [feature] db생성, 데이터 넣기
    todo : 만든 데이터들로 프리팹 만들어서 스크롤뷰에 붙이기

--- Assets/Scripts/UI/Monitor/ItemData.cs
+++ Assets/Scripts/UI/Monitor/ItemData.cs
@@ -7,6 +7,6 @@ public class ItemData : ScriptableObject
     public string displayName;
     public Sprite icon;
     public float unitPrice;
-    public ItemCategory category;
-    public DisplayType displayType;
+    // public ItemCategory category;
+    // public DisplayType displayType;
 }
