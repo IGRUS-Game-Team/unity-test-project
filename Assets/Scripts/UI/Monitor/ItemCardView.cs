58f025cf25b9f27692f5865e75ec01d48d8b5ce8
jeongminb933 <65400179+jeongminb933@users.noreply.github.com>
2025-07-22 18:53:59 +0900

    [feature] 데이터베이스에 있는 목록들 스크롤뷰에 넣기 구현

--- Assets/Scripts/UI/Monitor/ItemCardView.cs
+++ Assets/Scripts/UI/Monitor/ItemCardView.cs
@@ -5,27 +5,27 @@ using UnityEngine.UI;
 
 public class ItemCardView : MonoBehaviour
 {
-    [SerializeField] Image icon;
-    [SerializeField] TMP_Text nameText;
-    [SerializeField] TMP_Text unitPriceText;
-    [SerializeField] TMP_Text amountText;
-    [SerializeField] TMP_Text totalText;
-    [SerializeField] Button plusBtn, minusBtn, addBtn;
+   // [SerializeField] Image icon;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI unitPriceText;
+    [SerializeField] TextMeshProUGUI amountText;
+   // [SerializeField] TextMeshProUGUI totalText;
+   // [SerializeField] Button plusBtn, minusBtn, addBtn;
 
     private ItemData data;
     private int amount = 1;
 
-    public void Setup(ItemData item, Action<ItemData,int> onAddToCart)
+    public void Setup(ItemData item)
     {
         data = item;
-        icon.sprite = data.icon;
+      //  icon.sprite = data.icon;
         nameText.text = data.displayName;
         unitPriceText.text = $"${data.unitPrice:F2}";
         UpdateAmount(amount);
 
-        plusBtn.onClick.AddListener(()=>ChangeAmount(1));
-        minusBtn.onClick.AddListener(()=>ChangeAmount(-1));
-        addBtn.onClick.AddListener(()=> onAddToCart?.Invoke(data, amount));
+       // plusBtn.onClick.AddListener(()=>ChangeAmount(1));
+       // minusBtn.onClick.AddListener(()=>ChangeAmount(-1));
+       // addBtn.onClick.AddListener(()=> onAddToCart?.Invoke(data, amount));
     }
 
     void ChangeAmount(int delta)
@@ -37,6 +37,6 @@ public class ItemCardView : MonoBehaviour
     void UpdateAmount(int a)
     {
         amountText.text = a.ToString();
-        totalText.text  = $"${(data.unitPrice * a):F2}";
+    //    totalText.text  = $"${(data.unitPrice * a):F2}";
     }
 }
---
+++ Assets/Scripts/UI/Monitor/ShopUIController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUIController : MonoBehaviour
+{
+    [Header("Refs")]
+    [SerializeField] ItemDatabase database;
+    [SerializeField] Transform contentRoot;          // ScrollView/Viewport/Content
+    [SerializeField] ItemCardView itemCardPrefab;    // 프리팹
+
+    List<ItemCardView> spawnedCards = new();
+
+    void Start()
+    {
+        Populate();
+    }
+
+    public void Populate()
+    {
+        Clear();
+
+        foreach (var item in database.items)
+        {
+            var card = Instantiate(itemCardPrefab, contentRoot);
+            card.Setup(item);
+            spawnedCards.Add(card);
+        }
+    }
+
+    void Clear()
+    {
+        foreach (var c in spawnedCards)
+            Destroy(c.gameObject);
+        spawnedCards.Clear();
+    }
+}
